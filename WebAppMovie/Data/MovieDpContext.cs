e7206c3983c40b13830aa586b61f78985eb8994a
TatikondaAnvitha123 <tatikondaanvitha05@gmail.com>
2024-02-12 14:39:55 +0530

    Add project files.

---
+++ WebAppMovie/Data/MovieDpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppMovie.Models;
+
+namespace WebAppMovie.Data
+{
+    public class MovieDpContext : DbContext
+    {
+        public MovieDpContext (DbContextOptions<MovieDpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
